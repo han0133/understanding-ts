9c1c17553d2b314575ab7dcf5ea1fa64ecebe42d
Thor0111 <buddhaandai@gmail.com>
2025-02-08 02:23:55 +0900

    udemy0208

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-var person = {
-    name: "Maximilian",
-    age: 30,
-};
-console.log(person);
+"use strict";
+function combine(input1, input2) {
+    let result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+const combinedAges = combine(10, 26);
+console.log(combinedAges);
+const combinedNames = combine("max1", "Anna");
+console.log(combinedNames);
---
+++ objs-arrays.ts
@@ -0,0 +1,10 @@
+const person = {
+  name: "So-young",
+  age: 32,
+  hobbies: ["TV", "Writing"],
+  role: 4,
+};
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+}
